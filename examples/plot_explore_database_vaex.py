528383faff72c12068af921cf9c8e767a5064671
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2023-07-26 11:57:20 +0200

    merged two example in this one

--- examples/plot_explore_database_vaex.py
+++ examples/plot_explore_database_vaex.py
@@ -3,44 +3,76 @@
 .. _example_explore_database_vaex:
 
 ================================
-Explore Line Database Parameters
+Download and Explore Line Database
 ================================
 
-Database will be downloaded automatically and can be edited locally.
-
-The :ref:`Download HITEMP Database example <example_download_hitemp>` showed
-how to download the HITEMP database under Pandas format.
-
-RADIS can also be used to explore and visualize larger databases, using the
-underlying :py:mod:`vaex` library.
+Databases can be downloaded automatically and edited locally. By default the database
+is returned as a :py:mod:`pandas` DataFrame. To explore huge databases (like HITEMP CO2, CH4 or H2O, or ExoMol)
+that do not fit in RAM, RADIS allows you to use a :py:mod:`vaex` DataFrame instead (out-of-RAM).
 
 """
 
-from radis.io.hitemp import fetch_hitemp
+#%% Example with HITRAN and Pandas
+from radis.io.hitran import fetch_hitran
 
-df = fetch_hitemp("CO2", output="vaex", load_wavenum_min=2150, load_wavenum_max=2450)
-print(f"{len(df)} lines in HITEMP CO2; 2150 - 2450 cm-1")
+df_OH = fetch_hitran("OH")
+print(df_OH.columns)
+
 
 #%%
-# Note the use of `output='vaex'` in :py:func:`~radis.io.hitemp.fetch_hitemp` above.
-# The returned DataFrame is a Vaex DataFrame.
-# Loading times takes only few tens of milliseconds even for the largest HITEMP or ExoMol
-# databases
-#
+# Returns:
+# ::
+# Index(['id', 'iso', 'wav', 'int', 'A', 'airbrd', 'selbrd', 'El', 'Tdpair',
+#        'Pshft', 'gp', 'gpp'],
+#       dtype='object')
+
+#%% Example with HITEMP and vaex
+from radis.io.hitemp import fetch_hitemp
+
+df_CO2 = fetch_hitran(
+    "CO2",
+    output="pandas",  # default
+    load_wavenum_min=2000,
+    load_wavenum_max=2500,
+)
+df_CO2_hitemp = fetch_hitemp(
+    "CO2",
+    output="vaex",  # return a Vaex DataFrame.
+    load_wavenum_min=2000,
+    load_wavenum_max=2500,
+    columns="jl",  # a specific can be retrieved
+)
+print(f"{len(df_CO2_hitemp)} lines in HITEMP CO2; 2000 - 2450 cm-1")
+
+#%%
 # We can also use Vaex graph functions.
 # See Vaex vizualisations : https://vaex.readthedocs.io/en/latest/guides/advanced_plotting.html#
 #
-# For instance, we plot a :py:func:`~vaex.viz.DataFrameAccessorViz.heatmap` showing Self and Air-broadening coefficients
+# For instance, we plot here (left) the density of lines in the HITRAN and HITEMP databases and (right) the variations of the broadening coefficients with the rotational number.
 
 import matplotlib.pyplot as plt
 
-plt.figure()
-df.viz.heatmap("airbrd", "selbrd", limits="99%")
+fig_size = (13, 5)
+f = plt.figure(figsize=fig_size)
+ax_histo = f.add_subplot(1, 2, 1)
+df_CO2.viz.histogram("wav", color="k", label="HITRAN")
+df_CO2_hitemp.viz.histogram(
+    "wav",
+    color="r",
+    label="HITEMP",
+    xlabel="Wavenumber [cm$^{-1}$]",
+    ylabel="Counts",
+)
+ax_histo.set_ylim(bottom=0)
+ax_histo.legend()
 
-# TODO / idea : compare CO2 broadening with Air broadening for HITRAN database ?
-#%%
-# Or below we plot the number of lines using Vaex's :py:func:`~vaex.viz.DataFrameAccessorViz.histogram`
-
-plt.figure()
-df.viz.histogram("wav", shape=1000)
-plt.yscale("log")
+#% Figure 2
+ax_gamma_J = f.add_subplot(1, 2, 2)
+df_CO2_hitemp.viz.heatmap(
+    "jl",
+    "airbrd",
+    f="log",
+    limits="90%",
+    xlabel="Rotational number, J",
+    ylabel="$\gamma_{air}$ [cm$^{-1}$/atm]",
+)
